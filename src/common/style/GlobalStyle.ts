dc12a31393f298dc6cd49665828e7e339ce9d97d
HanYS <hys6078@naver.com>
2022-06-26 16:03:27 +0900

    Feat : 손님 메인화면 UI 작성(#4)

--- src/common/style/GlobalStyle.ts
+++ src/common/style/GlobalStyle.ts
@@ -6,6 +6,7 @@ const GlobalStyles = createGlobalStyle`
   margin: 0 0;
   padding:0 0;
   box-sizing: border-box;
+  font-family:'Neurit';
 }
 
 html,body, #root{
--- src/pages/GuestHome.tsx
+++ src/pages/GuestHome.tsx
@@ -1,28 +1,68 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 import BackgroundImg from '../assets/imgs/temp.png';
+import theme from '../common/style/theme';
 
 const GuestHomeWrapper = styled.div`
   width: 100%;
-  height: 100%;
+  //height: 100%;
   display: flex;
   flex-direction: column;
-  font-family: 'Neurit';
+  padding: 15px 15px;
+`;
+
+const TextWrapper = styled.div`
+  font-size: ${theme.calcRem(25)};
+  b {
+    font-size: ${theme.calcRem(28)};
+  }
 `;
 
 const BackgroundImage = styled.img`
   width: 100%;
-  height: 100%;
 `;
 
-const startBtn = styled.button``;
+const BtnArea = styled.div`
+  position: absolute;
+  bottom: 45px;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StartBtn = styled.button`
+  font-size: ${theme.calcRem(30)};
+  height: 2.5rem;
+  width: 80%;
+  border-radius: 12px;
+  background-color: #d9d9d9;
+  border: none;
+`;
 
 function GuestHome() {
+  const navigate = useNavigate();
+
+  const onStartClick = useCallback(() => {
+    navigate('/guest/selection');
+  }, []);
+
   return (
     <GuestHomeWrapper>
-      안녕
+      <TextWrapper>
+        <b>닉네임</b>&nbsp;님에게
+        <br />
+        <b>10</b> 송이의 꽃이 도착했어요.
+        <br />
+        2022.06.26
+      </TextWrapper>
       <BackgroundImage src={BackgroundImg} />
+      <BtnArea>
+        <StartBtn onClick={onStartClick}>꽃 보내기</StartBtn>
+      </BtnArea>
     </GuestHomeWrapper>
   );
 }
