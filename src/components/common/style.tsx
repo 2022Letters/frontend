e746aca29621a4e9bb200c8f72605272fab4d59c
k915k1004 <k915k1004@naver.com>
2022-06-27 14:15:25 +0900

    Etc : #3 스타일 공통 요소 파일 추가

---
+++ src/components/common/style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  height: 2.5rem;
+  width: 80%;
+  border: none;
+  border-radius: 12px;
+  margin-bottom: 45px;
+`;
