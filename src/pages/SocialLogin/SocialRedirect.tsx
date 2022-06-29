5b87ab3d993f4f466ea62a45906f81fffdde327e
k915k1004 <k915k1004@naver.com>
2022-06-29 23:04:39 +0900

    Refactor : 두번 렌더링 수정

--- src/pages/SocialLogin/SocialRedirect.tsx
+++ src/pages/SocialLogin/SocialRedirect.tsx
@@ -2,15 +2,21 @@ import axios from 'axios';
 import React, { useEffect } from 'react';
 
 function SocialRedirect() {
+  useEffect(() => {
+    callKakaoLogin();
+  }, []);
+
   const url = new URL(window.location.href);
 
   const params = url.searchParams;
   const code = params.get('code');
-  useEffect(() => {
+  const callKakaoLogin = () => {
     console.log(code);
     // http://localhost:8080/kakaoLogin
-    axios.get(`http://localhost:8080/kakaoLogin?code=${code}`);
-  });
+    axios.get(`http://localhost:8080/kakaoLogin?code=${code}`).then((res) => {
+      console.log(res);
+    });
+  };
 
   return <div>로딩중</div>;
 }
