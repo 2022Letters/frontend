ff46cb8d45b460f31802174783db98c32a9162c3
HanYS <hys6078@naver.com>
2022-07-01 03:13:05 +0900

    Feat : #16 꽃 배치 바운더리 제한 추가

--- src/pages/GuestLayout.tsx
+++ src/pages/GuestLayout.tsx
@@ -60,49 +60,73 @@ interface IBox {
 
 export default function GuestLayout() {
   const [pos, setPos] = useState<IPos>({ x: 0, y: 0 });
+  const [originPos, setOriginPos] = useState<IPos>({ x: 0, y: 0 });
   const [box, setBox] = useState<IBox>({ width: 0, height: 0 });
 
   const imgWrapper = useRef() as React.MutableRefObject<HTMLDivElement>;
+  const nodeRef = useRef(null);
   const categoryId = 1;
+  const imgSize = 100;
 
   useEffect(() => {
     const wrapperBox = imgWrapper.current.getBoundingClientRect();
+    setBox({ ...box, width: wrapperBox.width, height: wrapperBox.height });
     console.log(wrapperBox);
-    setBox({ width: wrapperBox.width, height: wrapperBox.height } as IBox);
-
-    const fetchData = async () => {
-      // const response = await axios.get(`${process.env.apiUrl}/api/post/${1}`);
-    };
-    fetchData();
+    // const fetchData = async () => {
+    //   const response = await axios.get(`${process.env.apiUrl}/api/post/${1}`);
+    // };
+    // fetchData();
   }, []);
 
+  const startPos = (data: any) => {
+    if (
+      data.x >= 0 &&
+      data.x + imgSize < box.width &&
+      data.y >= 0 &&
+      data.y + imgSize < box.height
+    ) {
+      setOriginPos({ ...originPos, x: data.x, y: data.y });
+    } else {
+      setPos({ ...pos, x: originPos.x, y: originPos.y });
+    }
+  };
+
   const trackPos = (data: any) => {
-    setPos((prev) => ({ ...prev, x: data.x, y: data.y } as IPos));
+    setPos({ ...pos, x: data.x, y: data.y });
   };
 
   const stopPos = (data: any) => {
-    // setPos({ x: data.x, y: data.y });
-    console.log(pos);
+    if (
+      data.x >= 0 &&
+      data.x + imgSize < box.width &&
+      data.y >= 0 &&
+      data.y + imgSize < box.height
+    ) {
+      setPos({ ...pos, x: data.x, y: data.y });
+    } else {
+      setPos({ ...pos, x: originPos.x, y: originPos.y });
+    }
   };
 
   const onFinishClick = useCallback(() => {
     const xRatio = pos.x / box.width;
     const yRatio = pos.y / box.height;
     console.log(pos, box, xRatio, yRatio);
-  }, []);
+  }, [pos, box]);
 
   return (
     <MainWrapper>
-      <MainTitle>
-        원하는 위치에 꽃을 배치해주세요.{pos.x} {pos.y}
-      </MainTitle>
+      <MainTitle>원하는 위치에 꽃을 배치해주세요.</MainTitle>
       <ImgWrapper ref={imgWrapper}>
         <UserImg src={flowerwraps[categoryId]} />
         <Draggable
+          position={pos}
+          onStart={(e, data) => startPos(data)}
           onDrag={(e, data) => trackPos(data)}
           onStop={(e, data) => stopPos(data)}
+          nodeRef={nodeRef}
         >
-          <SelectedLeaf src={leaves[categoryId][0].url} />
+          <SelectedLeaf src={leaves[categoryId][0].url} ref={nodeRef} />
         </Draggable>
       </ImgWrapper>
       <FinishBtn onClick={onFinishClick}>완료</FinishBtn>
